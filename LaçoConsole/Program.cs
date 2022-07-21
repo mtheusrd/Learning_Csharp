e834ddc6fda91929714a70fc3252bdbc9058e6b6
mtheusrd <mtheusrd1@gmail.com>
2022-07-21 17:32:26 +0100

    Novo trabalho - Laço

---
+++ LaçoConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Trabalho2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t Laço - Matheus");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                for (int j = 4; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
