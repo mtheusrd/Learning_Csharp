c7e0b7bdd87a84c8cf5e66e319fb9d3ac472ff09
mtheusrd <mtheusrd1@gmail.com>
2022-07-21 17:39:09 +0100

    Exercicio 3 e 4

---
+++ Matriz_ID/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Matriz_ID;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t  Matheus Gonçalves\n\tExercicio 4\n");
+
+            int tamanho;
+
+            Console.WriteLine("Informe o tamanho da Matrix Identidade: ");
+            tamanho = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n Matrix Identidade\n\n");
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
---
+++ While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace While_Matheus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t  Matheus Gonçalves\n\tExercicio 3\n");
+            char ch;
+            int i = 10;
+            do
+            {
+                Console.Write(i);
+                Console.Write(" ");
+                ch = (char)i;
+                Console.WriteLine(ch);
+                i++;
+            } while (i <= 255);
+            Console.ReadKey();
+        }
+    }
+}
+
