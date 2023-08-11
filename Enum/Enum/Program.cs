7f5b34cd2a65973395f5aa61d2fc4f606f41fb83
Matheus Gonçalves <67073149+mtheusrd@users.noreply.github.com>
2023-08-11 15:13:06 +0100

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        enum NotasDeReal_e
+        {
+            NotaDe2 = 2, 
+            NotaDe5 = 5,
+            NotaDe10 = 10
+        }
+
+        enum NotaDaProva_e
+        {
+            Nota0 = 0,
+            Nota1,
+            Nota2,
+            Nota3,
+            Nota4
+        }
+        static void Main(string[] args)
+        {
+            NotasDeReal_e minhaNota = NotasDeReal_e.NotaDe10;
+            Console.WriteLine(minhaNota + " vale" + Convert.ToUInt32(minhaNota));
+
+            NotaDaProva_e minhaNotaDaProva = NotaDaProva_e.Nota4;
+            Console.WriteLine(minhaNotaDaProva + " vale " + Convert.ToUInt32(minhaNotaDaProva));
+
+            Console.ReadKey();
+
+        }
+    }
+}
